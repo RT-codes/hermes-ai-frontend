4ac3d5357259fd1fd267c112a9e7c155d8045069
Rowan Tijsterman <rowan.nicolay@gmail.com>
2026-08-17 12:47:34 +0200

    add brain stage component

---
+++ src/components/BrainStage/BrainStage.tsx
@@ -0,0 +1,10 @@
+export function BrainStage() {
+  return (
+    <section className="brain-stage">
+      <div className="brain-placeholder">
+        <div className="brain-core">H</div>
+        <span>Brain visualization</span>
+      </div>
+    </section>
+  )
+}
